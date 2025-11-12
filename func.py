d29299208393e58766f8591759d9d50e3cdb6d2e
Matthias Anseaume <anseaume@kth.se>
2025-11-12 09:36:35 +0100

    fonction durée signaux

--- func.py
+++ func.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from os.path import basename, splitext, join
 import random
-
+import yann as pd
 
 def add_noise(s,noise,snr):
     """
@@ -11,7 +11,8 @@ def add_noise(s,noise,snr):
     on ajoute une partie aléatoire du bruit pour éviter d'avoir tout le temps le même bruit et éviter l'overfitting
     """
     # SNR entre -10 et 10 dB
-
+    #ajouter vérification de puissance équivalente entre bruit et signal
+    noise = noise/max(noise)
     s_len = len(s)
     n_len = len(noise)
     alpha = 10**(-snr/20)
@@ -20,6 +21,23 @@ def add_noise(s,noise,snr):
     x = s + alpha * noise
 
     return x
+
+def data_sized(x,time_limit):
+    """Fonction qui met la taille des signaux à time_limit sec
+    Ajoute des zéros si taille< time_limit sec et coupe le signal si > time_limit sec
+    """
+    Fs = 16000
+    sample_limit = Fs * time_limit
+    min_limit = sample_limit/2
+
+    if len(x) < sample_limit:
+        x_sized = x + np.zeros((sample_limit - len(x)),1)
+    else:
+        q = len(x)//sample_limit
+        x_sized = x[:q*sample_limit]
+        x_sized.reshape(q,sample_limit)
+    return x_sized
+
 
 
 
@@ -31,3 +49,4 @@ def STFTabs(y,hop_length,win_length,window,n_fft):
 
 def STFT_display():
     pass
+
